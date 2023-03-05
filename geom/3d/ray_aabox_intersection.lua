2dac87614f2a584aeb78fdb8427d429cf5bd50a8
Eike Decker <zet23t@googlemail.com>
2023-03-05 12:30:05 +0100

    Fixing the aabox raycast code

--- geom/3d/ray_aabox_intersection.lua
+++ geom/3d/ray_aabox_intersection.lua
@@ -16,7 +16,7 @@
 ---@return number|nil normal_x
 ---@return number|nil normal_y
 ---@return number|nil normal_z
-return function (ray_x, ray_y, ray_z, dir_x, dir_y, dir_z, min_x, min_y, min_z, max_x, max_y, max_z)
+return function(ray_x, ray_y, ray_z, dir_x, dir_y, dir_z, min_x, min_y, min_z, max_x, max_y, max_z)
 	local txmin, tymin, tzmin = (min_x - ray_x) / dir_x, (min_y - ray_y) / dir_y, (min_z - ray_z) / dir_z
 	local txmax, tymax, tzmax = (max_x - ray_x) / dir_x, (max_y - ray_y) / dir_y, (max_z - ray_z) / dir_z
 
@@ -30,31 +30,27 @@ return function (ray_x, ray_y, ray_z, dir_x, dir_y, dir_z, min_x, min_y, min_z, 
 	if tmax < 0 or tmin > tmax then return end
 
 	local hit_x, hit_y, hit_z
-	if tmin >= 0 then
-		if txmin == tmin then
-			-- hit front face
+	if txmin == tmin then
+		if dir_x > 0 then
 			hit_x, hit_y, hit_z = min_x, ray_y + tmin * dir_y, ray_z + tmin * dir_z
 			return hit_x, hit_y, hit_z, -1, 0, 0
-		elseif tymin == tmin then
-			-- hit bottom face
+		else
+			hit_x, hit_y, hit_z = max_x, ray_y + tmin * dir_y, ray_z + tmin * dir_z
+			return hit_x, hit_y, hit_z, 1, 0, 0
+		end
+	elseif tymin == tmin then
+		if dir_y > 0 then
 			hit_x, hit_y, hit_z = ray_x + tmin * dir_x, min_y, ray_z + tmin * dir_z
 			return hit_x, hit_y, hit_z, 0, -1, 0
 		else
-			-- hit left face
-			hit_x, hit_y, hit_z = ray_x + tmin * dir_x, ray_y + tmin * dir_y, min_z
-			return hit_x, hit_y, hit_z, 0, 0, -1
-		end
-	else
-		if txmax == tmin then
-			-- hit back face
-			hit_x, hit_y, hit_z = max_x, ray_y + tmin * dir_y, ray_z + tmin * dir_z
-			return hit_x, hit_y, hit_z, 1, 0, 0
-		elseif tymax == tmin then
-			-- hit top face
 			hit_x, hit_y, hit_z = ray_x + tmin * dir_x, max_y, ray_z + tmin * dir_z
 			return hit_x, hit_y, hit_z, 0, 1, 0
+		end
+	else
+		if dir_z > 0 then
+			hit_x, hit_y, hit_z = ray_x + tmin * dir_x, ray_y + tmin * dir_y, min_z
+			return hit_x, hit_y, hit_z, 0, 0, -1
 		else
-			-- hit right face
 			hit_x, hit_y, hit_z = ray_x + tmin * dir_x, ray_y + tmin * dir_y, max_z
 			return hit_x, hit_y, hit_z, 0, 0, 1
 		end
