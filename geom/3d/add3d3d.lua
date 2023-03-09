a80bd28dea11c69204c56674e3f39b7dae393dd1
Eike Decker <zet23t@googlemail.com>
2023-03-09 22:52:55 +0100

    vector adding helper

---
+++ geom/3d/add3d3d.lua
@@ -0,0 +1,12 @@
+---@param x1 number
+---@param y1 number
+---@param z1 number
+---@param x2 number
+---@param y2 number
+---@param z2 number
+---@return number x
+---@return number y
+---@return number z
+return function (x1,y1,z1, x2,y2,z2)
+	return x1 + x2, y1 + y2, z1 + z2
+end
