049bf8b7c123d802a928a234129222a63fe082ef
Eike Decker <zet23t@googlemail.com>
2023-03-19 08:52:45 +0100

    Modifying and fixing cylinder generator code to allow making cones

--- geom/3d/mesh/cylinder_mesh_generator.lua
+++ geom/3d/mesh/cylinder_mesh_generator.lua
@@ -11,14 +11,37 @@ function cylinder_mesh_generator:new(mat, radius, height, subdivisions, has_top,
 		subdivisions = subdivisions,
 		has_top = has_top,
 		has_bottom = has_bottom,
+		bottom_radius_scale = 1,
+		top_radius_scale = 1,
 		color = color or { 1, 1, 1, 1 }
 	}
+end
+
+function cylinder_mesh_generator:set_radius(radius)
+	self.radius = radius
+	return self
+end
+
+function cylinder_mesh_generator:set_height(height)
+	self.height = height
+	return self
+end
+
+function cylinder_mesh_generator:set_bottom_radius_scale(scale)
+	self.bottom_radius_scale = scale
+	return self
+end
+
+function cylinder_mesh_generator:set_top_radius_scale(scale)
+	self.top_radius_scale = scale
+	return self
 end
 
 ---@param mesh_builder mesh_builder
 function cylinder_mesh_generator:generate(mesh_builder)
 	local subdivs = self.subdivisions
-	local radius = self.radius
+	local radius_top = self.radius * self.top_radius_scale
+	local radius_bottom = self.radius * self.bottom_radius_scale
 	local half_height = self.height / 2
 	local prev_bottom, prev_top, prev_cap_top, prev_cap_bottom
 	local center_bottom, center_top = mesh_builder:allocate_vertices(2)
@@ -30,19 +53,30 @@ function cylinder_mesh_generator:generate(mesh_builder)
 		local u = i / subdivs
 		local angle = math.pi * i / subdivs * 2
 		local nx, nz = math.sin(angle), math.cos(angle)
-		local x, z = nx * radius, nz * radius
+		local ny = (radius_bottom - radius_top) / self.height
+		local x_top, z_top = nx * radius_top, nz * radius_top
+		local x_bottom, z_bottom = nx * radius_bottom, nz * radius_bottom
 		local bottom, top = mesh_builder:allocate_vertices(2)
 		-- print(bottom,top)
-		self:set_vertice(mesh_builder, bottom, x, -half_height, z, u, 0, nx, 0, nz)
-		self:set_vertice(mesh_builder, top, x, half_height, z, u, 1, nx, 0, nz)
+		self:set_vertice(mesh_builder, bottom, x_bottom, -half_height, z_bottom, u, 0, nx, ny, nz)
+		self:set_vertice(mesh_builder, top, x_top, half_height, z_top, u, 1, nx, ny, nz)
 		local cap_bottom, cap_top = mesh_builder:allocate_vertices(2)
 		local u, v = nx * .5 + .5, nz * .5 + .5
-		self:set_vertice(mesh_builder, cap_bottom, x, -half_height, z, u, v, 0, -1, 0)
-		self:set_vertice(mesh_builder, cap_top, x, half_height, z, u, v, 0, 1, 0)
+		self:set_vertice(mesh_builder, cap_bottom, x_bottom, -half_height, z_bottom, u, v, 0, -1, 0)
+		self:set_vertice(mesh_builder, cap_top, x_top, half_height, z_top, u, v, 0, 1, 0)
 		if i > 0 then
-			mesh_builder:add_triangles(cap_top, prev_cap_top, center_top)
-			mesh_builder:add_triangles(prev_cap_bottom, cap_bottom, center_bottom)
-			mesh_builder:add_triangles(prev_bottom, prev_top, top, prev_bottom, top, bottom)
+			if self.has_top and radius_top ~= 0 then
+				mesh_builder:add_triangles(cap_top, prev_cap_top, center_top)
+			end
+			if self.has_bottom and radius_bottom ~= 0 then
+				mesh_builder:add_triangles(prev_cap_bottom, cap_bottom, center_bottom)
+			end
+			if radius_top == 0 then
+				mesh_builder:add_triangles(prev_bottom, top, bottom)
+			elseif radius_bottom == 0 then
+			else
+				mesh_builder:add_triangles(prev_bottom, prev_top, top, prev_bottom, top, bottom)
+			end
 		end
 		prev_bottom, prev_top = bottom, top
 		prev_cap_bottom, prev_cap_top = cap_bottom, cap_top
