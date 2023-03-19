ec1e2e754ecefdc13bdaca957093442e46cdbca9
Eike Decker <zet23t@googlemail.com>
2023-03-19 08:52:10 +0100

    Updating lua docs

--- geom/3d/ray_plane_intersection.lua
+++ geom/3d/ray_plane_intersection.lua
@@ -1,3 +1,18 @@
+---@param ray_x number
+---@param ray_y number
+---@param ray_z number
+---@param dir_x number
+---@param dir_y number
+---@param dir_z number
+---@param plane_x number
+---@param plane_y number
+---@param plane_z number
+---@param normal_x number
+---@param normal_y number
+---@param normal_z number
+---@return number|nil x component (or nil when there is no intersection)
+---@return number|nil y component
+---@return number|nil z component
 return function(ray_x, ray_y, ray_z, dir_x, dir_y, dir_z, plane_x, plane_y, plane_z, normal_x, normal_y, normal_z)
 	local denominator = normal_x * dir_x + normal_y * dir_y + normal_z * dir_z
 
