3d5edf8d132a3aef37ad5fd241db75250602ba69
Eike Decker <zet23t@googlemail.com>
2023-03-23 00:06:03 +0100

    Adding optional scaling factore to normalize code
    ... in case a special scale is desired

--- geom/3d/normalize3d.lua
+++ geom/3d/normalize3d.lua
@@ -1,6 +1,6 @@
-return function(x, y, z)
+return function(x, y, z, scale)
 	local d = x * x + y * y + z * z
 	if d == 0 then return 0, 0, 0, 0 end
-	d = d ^ .5
+	d = (d ^ .5) / (scale or 1)
 	return x / d, y / d, z / d, d
 end
