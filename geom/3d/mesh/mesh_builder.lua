499a6b2c7a5e6e36b39e8a11153f4e972bfa09a3
Eike Decker <zet23t@googlemail.com>
2023-03-24 21:11:00 +0100

    Adding mesh normal recalculation algorithm
    the optimization part is missing; I don't need that one yet. For now
    it's nice that it looks nice (and correct).

--- geom/3d/mesh/mesh_builder.lua
+++ geom/3d/mesh/mesh_builder.lua
@@ -10,6 +10,10 @@
 ---@field color_size integer
 ---@field triangles integer[]
 local mesh_builder = require "love-util.class" "mesh_builder"
+local cross        = require "love-math.geom.3d.cross"
+local normalize3d  = require "love-math.geom.3d.normalize3d"
+local vector_angle = require "love-math.geom.3d.vector_angle"
+local length3d     = require "love-math.geom.3d.length3d"
 
 function mesh_builder:new()
 	return self:create {
@@ -33,15 +37,15 @@ end
 
 local function setVertexAttribute(mesh, vertice_count, attribute_index, data_size, data)
 	for i = 1, vertice_count do
-		mesh:setVertexAttribute(i, attribute_index, 
-		unpack_number_range(data, (i - 1) * data_size + 1, i * data_size))
+		mesh:setVertexAttribute(i, attribute_index,
+			unpack_number_range(data, (i - 1) * data_size + 1, i * data_size))
 	end
 end
 
 ---Returns a new vertice that can be used for vertice data
 ---@param amount integer|nil the number of vertices to allocate
 ---@return integer
----@return ... 
+---@return ...
 function mesh_builder:allocate_vertices(amount)
 	self.vertice_counter = self.vertice_counter + 1
 	if amount and amount > 1 then
@@ -51,11 +55,29 @@ function mesh_builder:allocate_vertices(amount)
 	return self.vertice_counter
 end
 
+local function get_data(data, index, size)
+	local idx = (index - 1) * size + 1
+	return unpack(data, idx, idx + size - 1)
+end
+
+function mesh_builder:allocate_from_vertice(source_id)
+	self.vertice_counter = self.vertice_counter + 1
+	local new_id = self.vertice_counter
+	self:set_position(new_id, get_data(self.vertices, source_id, self.vertice_size))
+	self:set_normal(new_id, get_data(self.normals, source_id, self.normal_size))
+	self:set_color(new_id, get_data(self.colors, source_id, self.color_size))
+	self:set_uv(new_id, get_data(self.uvs, source_id, self.uv_size))
+	return new_id
+end
+
 local function set_data(vertice_id, size, list, ...)
 	if size <= 0 then return end
 	local idx = (vertice_id - 1) * size
-	for i=1,size do
-		list[idx + i] = select(i, ...)
+	for i = 1, size do
+		local num = select(i, ...)
+		-- nan check
+		assert(num == num)
+		list[idx + i] = num
 	end
 end
 
@@ -98,19 +120,104 @@ end
 ---@param ... integer
 ---@return mesh_builder
 function mesh_builder:add_triangles(vertice_id_a, vertice_id_b, vertice_id_c, ...)
-	self.triangles[#self.triangles+1] = vertice_id_a
-	self.triangles[#self.triangles+1] = vertice_id_b
-	self.triangles[#self.triangles+1] = vertice_id_c
+	self.triangles[#self.triangles + 1] = vertice_id_a
+	self.triangles[#self.triangles + 1] = vertice_id_b
+	self.triangles[#self.triangles + 1] = vertice_id_c
 	if ... then
 		return self:add_triangles(...)
 	end
 	return self
 end
 
+local function to_zero(x)
+	-- signed zeros are the worst
+	return math.abs(x) < 0.00001 and 0 or x
+end
+
+local function cut_bits(x, y, z)
+	x,y,z = to_zero(x), to_zero(y), to_zero(z)
+	return math.floor(x * 0x10000), math.floor(y * 0x10000), math.floor(z * 0x10000)
+end
+
+function mesh_builder:recalculate_normals(max_angle_radians)
+	local vertice_pos_info = {}
+	local triangle_normals = {}
+
+	local function add_triangle_vertice(vertice_id, x, y, z, triangle_id, nx, ny, nz)
+		x, y, z = cut_bits(x, y, z)
+		local pos_key = x .. " " .. y .. " " .. z
+		local v = vertice_pos_info[pos_key]
+		local tid = (triangle_id - 1) * 3 + 1
+		triangle_normals[tid] = nx
+		triangle_normals[tid + 1] = ny
+		triangle_normals[tid + 2] = nz
+		if not v then
+			vertice_pos_info[pos_key] = { triangle_id, vertice_id }
+		else
+			v[#v + 1] = triangle_id
+			v[#v + 1] = vertice_id
+		end
+		return add_triangle_vertice
+	end
+
+	for i = 1, #self.triangles, 3 do
+		local a, b, c = unpack(self.triangles, i, i + 2)
+		local ia, ib, ic = (a - 1) * 3 + 1, (b - 1) * 3 + 1, (c - 1) * 3 + 1
+		local ax, ay, az = unpack(self.vertices, ia, ia + 2)
+		local bx, by, bz = unpack(self.vertices, ib, ib + 2)
+		local cx, cy, cz = unpack(self.vertices, ic, ic + 2)
+		local nx, ny, nz = normalize3d(cross(bx - cx, by - cy, bz - cz, ax - cx, ay - cy, az - cz))
+
+		add_triangle_vertice(a, ax, ay, az, i, nx, ny, nz)(b, bx, by, bz, i + 1, nx, ny, nz)(c, cx, cy, cz, i + 2, nx, ny,
+			nz)
+	end
+
+	for k, v in pairs(vertice_pos_info) do
+		if #v > 2 then
+			local nx, ny, nz = 0, 0, 0
+			for i = 1, #v, 2 do
+				local id = (v[i] - 1) * 3 + 1
+				local tnx, tny, tnz = unpack(triangle_normals, id, id + 2)
+				nx, ny, nz = nx + tnx, ny + tny, nz + tnz
+			end
+			nx, ny, nz = normalize3d(nx, ny, nz)
+			for i = 1, #v, 2 do
+				local triangle_id, vertex_index = v[i], v[i + 1]
+				local atid = (triangle_id - 1) * 3 + 1
+				local atnx, atny, atnz = unpack(triangle_normals, atid, atid + 2)
+				local nx, ny, nz = atnx, atny, atnz
+				for j = 1, #v, 2 do
+					if j ~= i then
+						local btid = (v[j] - 1) * 3 + 1
+						local btnx, btny, btnz = unpack(triangle_normals, btid, btid + 2)
+						local angle = vector_angle(0, 0, 0, atnx, atny, atnz, btnx, btny, btnz)
+						if angle < max_angle_radians then
+							nx, ny, nz = nx + btnx, ny + btny, nz + btnz
+						end
+					end
+				end
+				nx, ny, nz = normalize3d(nx, ny, nz)
+				local new_id = self:allocate_from_vertice(vertex_index)
+				self.triangles[triangle_id] = new_id
+				self:set_normal(new_id, nx, ny, nz)
+			end
+		end
+	end
+
+	return self:optimize()
+end
+
+function mesh_builder:optimize()
+	-- TODO: 
+	-- - delete vertices not used by triangles
+	-- - merge vertices with nearly same position / uv / color / normal
+	return self
+end
+
 ---creates a new mesh using the data from the inputs
 ---@return love.Mesh
 function mesh_builder:create_mesh()
-	local vertice_count = self.vertice_counter 
+	local vertice_count = self.vertice_counter
 	-- assert(vertice_count%1 == 0, "vertice_data_count = "..vertice_count.."; "..#self.vertices)
 	local attribute_normal_index, attribute_uv_index, attribute_color_index
 	local attributes = { { "VertexPosition", "float", self.vertice_size } }
@@ -130,17 +237,17 @@ function mesh_builder:create_mesh()
 		attribute_color_index = #attributes
 	end
 
-	local mesh = love.graphics.newMesh(attributes, math.max(1, #self.vertices), "triangles", "static")
+	local mesh = love.graphics.newMesh(attributes, math.max(1, self.vertice_counter), "triangles", "static")
 	setVertexAttribute(mesh, vertice_count, 1, self.vertice_size, self.vertices)
 	setVertexAttribute(mesh, vertice_count, attribute_normal_index, self.normal_size, self.normals)
 	setVertexAttribute(mesh, vertice_count, attribute_uv_index, self.uv_size, self.uvs)
 	setVertexAttribute(mesh, vertice_count, attribute_color_index, self.color_size, self.colors)
-	if #self.triangles > 0 then 
+	if #self.triangles > 0 then
 		mesh:setVertexMap(self.triangles)
 	else
 		mesh:setDrawRange()
 	end
-	
+
 	return mesh
 end
 
